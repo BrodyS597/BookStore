6d3618e559175a8f1054628c06ed1a93d2e23eed
Brody Sears <brodysears@Brodys-Mac-mini.local>
2022-01-20 15:55:20 -0700

    Added tableviews and labels

---
+++ BookStore/BookStore/Controller/Custom Views/LibraryCellTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  LibraryCellTableViewCell.swift
+//  BookStore
+//
+//  Created by Brody Sears on 1/20/22.
+//
+
+import UIKit
+
+class LibraryCellTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
